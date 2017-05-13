71e862aaf20711885fd7efb99f4e72b4c84a869c
root <root@besnbirk.local>
2017-05-13 09:28:19 +0000

    firest commit

---
+++ manifests/site.pp
@@ -0,0 +1,14 @@
+package {'epel-release-6-8.noarch':
+   provider => 'rpm',
+   source => "http://ftp.linux.org.tr/epel/6/x86_64/epel-release-6-8.noarch.rpm",
+   ensure=>'installed'
+  }
+package { 'remi-release.noarch':
+   provider => 'rpm',
+   source => "http://rpms.famillecollet.com/enterprise/remi-release-6.rpm",
+   ensure =>'installed'
+  }
+package { 'nano': ensure => installed }
+package { 'wget': ensure => installed }
+package { 'htop': ensure => installed }
+
